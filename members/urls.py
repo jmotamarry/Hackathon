a714848b87033c731c73802a45365d11deb0b97c
Jay Motamarry <79808791+jmotamarry@users.noreply.github.com>
2023-11-15 19:21:34 -0800

    Revert "add moderator"

--- members/urls.py
+++ members/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     path ('login_user/', views.login_user, name='login'),
     path ('logout_user/', views.logout_user, name='logout'),
     path ('register_user/', views.register_user, name='register_user'),
-    path ('moderator/', views.moderator_view, name='moderator'),
-    path ('moderator/update/<int:id>/', views.moderator_update_view),
-    path ('moderator/delete/<int:id>/', views.moderator_delete_view),
-    path ('moderator/view/<int:id>/', views.moderator_event_detail_view),
 ]
--- members/views.py
+++ members/views.py
@@ -1,19 +1,16 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-from events.models import Event
 
 # Create your views here.
-def login_user(request):        # directs a user to a form to log in
+def login_user(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, ("You have successfully logged in"))
             return redirect('home')
         else:
             messages.success(request, ("There was an error logging in, Try again"))
@@ -23,12 +20,12 @@ def login_user(request):        # directs a user to a form to log in
     else:
         return render(request, 'authenticate/login.html', {})
     
-def logout_user(request):           # logs out a user
+def logout_user(request):
     logout(request)
     messages.success(request, ("Logged Out Successfully"))
     return redirect('/event/board/')
 
-def register_user(request):         # directs a user to a form to create account
+def register_user(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -38,35 +35,7 @@ def register_user(request):         # directs a user to a form to create account
             user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, ("Account creation successful"))
-            return redirect("/event/board/")
+            return redirect("/events/board/")
     else:
         form = UserCreationForm()
     return render(request, 'authenticate/register_user.html', {'form': form})
-
-def moderator_view(request):        # directs to a page for only moderators
-    sorted_list = Event.objects.all().order_by('date', 'start_time')
-    return render(request, 'authenticate/moderator.html', { 'object_list': sorted_list })
-
-def moderator_event_detail_view(request, id):           # shows one event
-    obj = get_object_or_404(Event, id=id)               # get object id=id, if it does not exist throw 404 error
-    context = {                                         # defines what to pass in to event_detail.html
-        'object': obj,
-    }
-    return render(request, 'authenticate/moderator_event_detail.html', context)
-    
-def moderator_update_view(request, id=id):
-    obj = get_object_or_404(Event, id=id)
-    obj.approved = True
-    obj.save(update_fields=['approved'])
-    messages.success(request, ("Event Approved"))
-    return redirect('/members/moderator/')
-
-def moderator_delete_view(request, id=id):
-    obj = get_object_or_404(Event, id=id)
-    if request.method == 'POST':
-        obj.delete()                        # confirm the user wants to delete the object
-        return redirect('/members/moderator/')
-    context = {
-        'object': obj,
-    }
-    return render(request, 'events/event_delete.html', context)
